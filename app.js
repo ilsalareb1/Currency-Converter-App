6b30c6d84cfcf7e5a5f8e81dfbe2d34f4826ba6e
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-12-02 11:09:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+export const DOMStrings = {
+    CurrentCurrency : document.getElementById("name-cc"),
+    WantedCurrency : document.getElementById("name-wc"),
+    AmountofCC : document.getElementById("amount"),
+    
+};
+
