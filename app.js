709224ecd1dc2a15b920480bd9c4188348df452f
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-12-02 11:45:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 export const DOMStrings = {
-    CurrentCurrency : document.getElementById("name-cc"),
-    WantedCurrency : document.getElementById("name-wc"),
-    AmountofCC : document.getElementById("amount"),
-    
+    InputCurrency : document.getElementById("CurrentCurrency"),
+    InputWanted: document.getElementById("WantedCurrency"),
+    InputAmount: document.getElementById("amount"),
+    ConvertedAmount: document.getElementsByClassName("display-amount-wc"),
+    ConvertedCurrency: document.getElementsByClassName("display-name-wc")
 };
 
+export function getCurrencyInfo (){
+    DOMStrings.InputAmount.addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        if(DOMStrings.InputAmount.value == ""){
+            alert("Please input all values!");
+        }
+
+        try{
+            const exchangeRate = await fetch (`http://data.fixer.io/api/latest?access_key=aa7cbda9baeeeda7532c0f97afaafdc6`);
+            
+            DOMstrings.ConvertedAmount.value = function converter() {
+                exchangeRate * DOMStrings.InputAmount.value
+            };
+        }
+
+    });
+}
